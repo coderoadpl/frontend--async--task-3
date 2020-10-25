d6b4a747101dc442e5c72394258d73ebc2d8b8b2
Mateusz Choma <kontakt@coderoad.pl>
2020-10-25 11:11:59 +0100

    Introducting callback after load

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ document.body.appendChild(script)
 
 // ReferenceError: add is not defined
 // console.log(add)
+
+script.addEventListener(
+    'load',
+    () => {
+        console.log(add)
+        console.log(add(1, 2))
+    }
+)
+
+console.log('I will be first!')
