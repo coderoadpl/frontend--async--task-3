2824eec9b0b8a78826575c31165e4863d611ac07
Mateusz Choma <kontakt@coderoad.pl>
2020-10-25 11:26:11 +0100

    Callbacks inside callbacks

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const loadScript = (src, callback) => {
 
     const script = document.createElement('script')
     script.src = src
-    
+
     script.addEventListener(
         'load',
         callback
@@ -12,7 +12,18 @@ const loadScript = (src, callback) => {
 
 }
 
+const runCalculations = () => {
+    const addResult = add(10, 10) // 20
+    const subtractResult = subtract(addResult, 4) // 16
+    const multiplyResult = multiply(subtractResult, 1) // 16
+    const divideResult = divide(multiplyResult, 2) // 8
+    console.log(divideResult)
+}
+
 loadScript('./add.js', () => {
-    console.log(add)
-    console.log(add(1, 2))
+    loadScript('./subtract.js', () => {
+        loadScript('./multiply.js', () => {
+            loadScript('./divide.js', runCalculations)
+        })
+    })
 })
